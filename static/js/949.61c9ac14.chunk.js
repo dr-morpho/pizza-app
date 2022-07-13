16b14decc6aa51c1fc281de9e844b430208b4e13
Jimbo <rodionminaev96@gmail.com>
2022-07-13 10:49:46 +0300

    Updates

---
+++ static/js/949.61c9ac14.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[949],{4949:function(u,e,n){n.r(e);n(2791);var p=n(6662),a=n(3329);e.default=function(){return(0,a.jsx)(p.Z,{})}}}]);
+//# sourceMappingURL=949.61c9ac14.chunk.js.map
